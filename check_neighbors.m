707fc6103a0e62ece66d412d17f9e7125fbda91f
miriamarden <79770159+miriamarden@users.noreply.github.com>
2021-03-02 16:38:21 -0500

    Update check_neighbors.m

--- check_neighbors.m
+++ check_neighbors.m
@@ -1,55 +1,58 @@
-function [] = check_neighbors()
+function [new_gen] = check_neighbors(current_gen)
 
-
+new_gen = zeros(100);
 neighbors = 0;
 for m = 2:99 %for rows 1 through 100
 	for n = 2:99 %for columns 1 through 100
-		if initial_gen(m,n) == 1
+		if current_gen(m,n) == 1
 			alive = 1; %If a coordinate is a live cell, the variable “alive” is true
        		else %if the coordinate is a dead cell
 			alive = 0; %If a coordinate is a dead cell, the variable “dead” is true
        	 	end
 
 
-		if initial_gen(m-1,n-1) == 1
+		if current_gen(m-1,n-1) == 1
 			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m-1,n) == 1
+		if current_gen(m-1,n) == 1
 			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m-1,n+1) == 1
+		if current_gen(m-1,n+1) == 1
    			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m,n-1) == 1
+		if current_gen(m,n-1) == 1
 			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m,n+1) == 1
+		if current_gen(m,n+1) == 1
 			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m+1,n-1) == 1
+		if current_gen(m+1,n-1) == 1
 			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m+1,n) == 1
+		if current_gen(m+1,n) == 1
 			neighbors = neighbors+1;
 		end
 	
-		if initial_gen(m+1,n+1) == 1
+		if current_gen(m+1,n+1) == 1
    			neighbors = neighbors+1;
 		end
 
 
 		if alive == 1 && (neighbors < 2 || neighbors >3)
-			initial_gen(m,n) = 0;
+			new_gen(m,n) = 0;
+		end
+		if alive == 1 && (neighbors == 2 || neighbors == 3)
+   			new_gen(m,n) = 1;
 		end
 	
 		if alive == 0 && neighbors == 3
-			initial_gen(m,n) = 1;
+			new_gen(m,n) = 1;
 		end
 
 
