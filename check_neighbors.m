cf9c6dcd9fec8dda9f9e1e034349a6f715e4d66b
Matt Murphy <77451610+matt-j-murphy@users.noreply.github.com>
2021-03-02 18:57:30 -0500

    Update check_neighbors.m

--- check_neighbors.m
+++ check_neighbors.m
@@ -1,19 +1,12 @@
 function [new_gen] = check_neighbors(current_gen)
 
 new_gen = zeros(100);
-neighbors = 0;
 for m = 2:99 %for rows 1 through 100
 	for n = 2:99 %for columns 1 through 100
-		if current_gen(m,n) == 1
-			alive = 1; %If a coordinate is a live cell, the variable “alive” is true
-       		else %if the coordinate is a dead cell
-			alive = 0; %If a coordinate is a dead cell, the variable “dead” is true
-       	 	end
-
-
-		if current_gen(m-1,n-1) == 1
-			neighbors = neighbors+1;
-		end
+        neighbors = 0;
+        if current_gen(m-1,n-1) == 1
+            neighbors = neighbors+1;
+        end
 	
 		if current_gen(m-1,n) == 1
 			neighbors = neighbors+1;
@@ -37,28 +30,33 @@ for m = 2:99 %for rows 1 through 100
 	
 		if current_gen(m+1,n) == 1
 			neighbors = neighbors+1;
-		end
-	
-		if current_gen(m+1,n+1) == 1
-   			neighbors = neighbors+1;
-		end
-
-
-		if alive == 1 && (neighbors < 2 || neighbors >3)
-			new_gen(m,n) = 0;
-		end
-		if alive == 1 && (neighbors == 2 || neighbors == 3)
-   			new_gen(m,n) = 1;
-		end
-	
-		if alive == 0 && neighbors == 3
-			new_gen(m,n) = 1;
-		end
-
-
-	end
+        end
+        
+        if current_gen(m+1,n+1) == 1
+            neighbors = neighbors+1;
+        end
+        
+        if current_gen(m,n) == 1
+            if (neighbors < 2 || neighbors >3)
+                new_gen(m,n) = 0;
+            end
+            
+            if (neighbors == 2 || neighbors == 3)
+                new_gen(m,n) = 1;
+            end 
+        elseif current_gen(m,n) == 0
+            
+            if neighbors == 3
+                new_gen(m,n) = 1;
+            else
+                new_gen(m,n) = 0;
+            end
+            
+        end
+    end
 end
 
-disp(initial_gen)
-
+show_stuff(new_gen);
+colormap gray
+axis off
 end
