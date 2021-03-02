15609752ec853f2b75ce371444cdc3ab2360b5b4
Matt Murphy <77451610+matt-j-murphy@users.noreply.github.com>
2021-03-02 18:57:57 -0500

    Update game_of_life.m

--- game_of_life.m
+++ game_of_life.m
@@ -1,12 +1,11 @@
-function [] = game_of_life()
-current_gen = randi([0,1],100)  %creates an initial generation matrix of random 0’s and 1’s
 
-colormap gray
+function [] = game_of_life()
+current_gen = randi([0,1],100);
+imagesc(current_gen); %creates an initial generation matrix of random 0’s and 1’s
 
 while true
     [new_gen] = check_neighbors(current_gen);
-    imagesc(new_gen);
+    current_gen = new_gen;
 end
 
 end
-
