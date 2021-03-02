ddeacb2670cf4f8b0ba996c52e42cb7831d0eed1
miriamarden <79770159+miriamarden@users.noreply.github.com>
2021-03-02 16:40:00 -0500

    Update game_of_life.m

--- game_of_life.m
+++ game_of_life.m
@@ -1,9 +1,11 @@
 function [] = game_of_life()
-initial_gen = randi([0,1],100)  %creates an initial generation matrix of random 0’s and 1’s
+current_gen = randi([0,1],100)  %creates an initial generation matrix of random 0’s and 1’s
+
+colormap gray
 
 while true
-    check_neighbors(initial_gen);
-    imagesc(initial_gen);
+    [new_gen] = check_neighbors(current_gen);
+    imagesc(new_gen);
 end
 
 end
