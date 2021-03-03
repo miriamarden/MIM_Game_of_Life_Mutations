65ffd7bc25b5382b9542a5eaab93627a0ad32273
Matt Murphy <77451610+matt-j-murphy@users.noreply.github.com>
2021-03-02 19:23:09 -0500

    Update check_neighbors.m

--- check_neighbors.m
+++ check_neighbors.m
@@ -4,7 +4,7 @@ new_gen = zeros(100);
 for m = 2:99 %for rows 1 through 100
 	for n = 2:99 %for columns 1 through 100
         neighbors = 0;
-        	if current_gen(m-1,n-1) == 1
+		if current_gen(m-1,n-1) == 1
             		neighbors = neighbors+1;
         	end
 	
@@ -37,23 +37,22 @@ for m = 2:99 %for rows 1 through 100
         	end
         
        
-        if current_gen(m,n) == 1
-            if (neighbors < 2 || neighbors >3)
-                new_gen(m,n) = 0;
-            end
+        	if current_gen(m,n) == 1
+			if (neighbors < 2 || neighbors >3)
+                		new_gen(m,n) = 0;
+            		end
             
-            if (neighbors == 2 || neighbors == 3)
-                new_gen(m,n) = 1;
-            end 
-        elseif current_gen(m,n) == 0
+            		if (neighbors == 2 || neighbors == 3)
+                		new_gen(m,n) = 1;
+            		end 
+        	elseif current_gen(m,n) == 0
+            		if neighbors == 3
+                		new_gen(m,n) = 1;
+            		else
+                		new_gen(m,n) = 0;
+            		end
             
-            if neighbors == 3
-                new_gen(m,n) = 1;
-            else
-                new_gen(m,n) = 0;
-            end
-            
-        end
+        	end
     end
 end
 
