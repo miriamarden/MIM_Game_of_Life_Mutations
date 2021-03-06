a64a7015301905d4d1d6b1926945ff68a0810f67
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-06 17:21:37 -0500

    Update show_stuff.m
    Added commentary and did research behind "drawnow" syntax, also debugged. This took me 30 minutes in total to do.

--- show_stuff.m
+++ show_stuff.m
@@ -1,6 +1,6 @@
 function [] = show_stuff(new_gen)
 imagesc(new_gen);
-colormap gray
+colormap gray %Game of Life will display in black & white, cells will have no color.
 axis off
-drawnow
+drawnow %updates the figure and displays game output.
 end
