83c699f5b54c10d313af9d8e88498961e479827c
miriamarden <79770159+miriamarden@users.noreply.github.com>
2021-03-08 23:03:02 -0500

    Delete show_stuff.m

--- show_stuff.m
+++
@@ -1,6 +0,0 @@
-function [] = show_stuff(new_gen)
-imagesc(new_gen);
-colormap gray %Game of Life will display in black & white, cells will have no color.
-axis off
-drawnow %updates the figure and displays game output.
-end
